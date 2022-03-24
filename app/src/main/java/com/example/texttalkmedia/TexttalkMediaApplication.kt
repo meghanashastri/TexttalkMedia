ed070183c71781a58b45a4c63197e34d9b0c1c03
meghanashastri <meghanashastri07@gmail.com>
2022-03-24 15:35:35 +0530

    basic folder structure created

---
+++ app/src/main/java/com/example/texttalkmedia/TexttalkMediaApplication.kt
@@ -0,0 +1,8 @@
+package com.example.texttalkmedia
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class TexttalkMediaApplication : Application() {
+}
